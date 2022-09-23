b738e94afafe425a9d1f4bb81eded1fe060f1fd7
yim monirak <52792657+monirak@users.noreply.github.com>
2022-09-23 18:55:53 +0700

    Add files via upload

---
+++ UC/js/read_size_device.js
@@ -0,0 +1,4 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+var x = document.getElementById("size_device").height = h-150;
+var y = document.getElementById("size_device").width = w-10;
---
+++ UC/js/readyear.js
@@ -0,0 +1,2 @@
+const d = new Date();
+document.getElementById("readyear").innerHTML = "© 2003 - " + d.getFullYear();
